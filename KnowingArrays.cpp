59cb5a7a52c0c6553077c34b2f63e8372efbb475
Knightususa <vanya.yakovlev2008@gmail.com>
2026-09-07 12:01:22 +0300

    Add struct type  for array

--- KnowingArrays.cpp
+++ KnowingArrays.cpp
@@ -3,46 +3,48 @@
 #include <stdbool.h>
 
 #ifndef NDEBUG
-
-#define yaissert(usl, text)                       \
-{                                                 \
-    if(!(usl))                                    \
-    {                                             \
-        printf("text\n%s:%s", __FILE__, __LINE__);\
-        abort();                                  \
-    }                                             \
-}
-
+    #define yaissert(usl, text)                       \
+    {                                                 \
+        if(!(usl))                                    \
+        {                                             \
+            printf("text\n%s:%s", __FILE__, __LINE__);\
+            abort();                                  \
+        }                                             \
+    }
 #else
     #define yaissert
 #endif
 
-bool    PrintHowItLooksLike (int* treangleArr, int Y, int X);
+typedef struct {
+    int* ref;
+    int len;
+} Triangle_array;
 
-int*    CreateTreangleArr (int* treangleArr, int y, int x);
+bool             PrintHowItLooksLike (Triangle_array triangleArr, int Y, int X);
 
-char     TAGetValue (int* treangleArr, int y, int x);//TreangleArrayGetValue
-void    TAEditValue (int* treangleArr, int y, int x, int value);//TreangleArrayEditValue
+Triangle_array   CreateTriangleArr (int y, int x);
+
+char             TAGetValue  (Triangle_array triangleArr, int y, int x);//TriangleArrayGetValue
+void             TAEditValue (Triangle_array triangleArr, int y, int x, int value);//TriangleArrayEditValue
 
 int main()
 {
-    PrintHowItLooksLike(NULL, 10, 10);
-    int treangleArr1[] = {};
-    CreateTreangleArr(treangleArr1, 5, 10);
-    TAEditValue(treangleArr1, 1, 1, 1);
-    TAEditValue(treangleArr1, 5, 5, 1);
-    PrintHowItLooksLike(treangleArr1, 5, 10);
-    return 1;
+    Triangle_array triangleArr1 = {.ref = NULL, .len = 0};
+    triangleArr1 = CreateTriangleArr(5, 10);
+
+    TAEditValue(triangleArr1, 1, 1, 1);
+    TAEditValue(triangleArr1, 5, 5, 1);
+    PrintHowItLooksLike(triangleArr1,  5, 10);
 }
 
-bool PrintHowItLooksLike (int* treangleArr, int Y, int X)
+bool PrintHowItLooksLike (Triangle_array triangleArr, int Y, int X)
 {
     printf("\n\nThis is how this array look like\n\n");
     for(int y = 0; y < Y * 2 + 1; y++)
     {
         for(int x = 0; x < X; x++)
         {
-            char a = (x * 2 - y < 0) ? TAGetValue(treangleArr, y - (1 + y / 2), x) : 'x'; 
+            char a = (x * 2 - y < 0) ? TAGetValue(triangleArr, y - (1 + y / 2), x) : 'x'; 
             if(y % 2 == 0)
                 printf("+-");
             else
@@ -57,35 +59,43 @@ bool PrintHowItLooksLike (int* treangleArr, int Y, int X)
     return 1;
 }
 
-char TAGetValue (int* treangleArr, int y, int x)
+char TAGetValue (Triangle_array triangleArr, int y, int x)
 {
-    if(treangleArr == NULL || x > y)
+    if(triangleArr.ref == NULL || x > y)
     {
         return ' ';
     }
-    return (char) ('0' + treangleArr[(((y + 1) * y) / 2) + x]);
+
+    int d = (((y + 1) * y) / 2) + x;
+    yaissert(0 <= d && d <= triangleArr.len, "Not in array\n");
+    return (char) ((int) '0' + triangleArr.ref[d]);
 }
 
-int* CreateTreangleArr (int* treangleArr, int y, int x)
-{
-    if(x < y)
-    {
-        printf("x must to be >= y for treangle type array\n");
-        return NULL;
-    }
+Triangle_array CreateTriangleArr (int Y, int X)
+{   
+    Triangle_array triangleArr = {.ref = NULL, .len = 0};
 
-    for(int d = 0; d < ((y + 1) * y + y * 2)/2; d++)
+    if(X <= 0 || Y <= 0)
     {
-        treangleArr[d] = 0;
+        printf("x and y must be positive");
+        return triangleArr;
     }
-    return treangleArr;
+    if(X < Y)
+    {
+        printf("x must to be >= y for triangle type array\n");
+        return triangleArr;
+    }
+    triangleArr.len = (((Y + 1) * Y + Y * 2)/2);
+    triangleArr.ref = (int*) calloc(triangleArr.len, sizeof(int));
+
+    return triangleArr;
 }
 
-void TAEditValue (int* treangleArr, int y, int x, int value)
+void TAEditValue (Triangle_array triangleArr, int y, int x, int value)
 {
     y--;
     x--;
-    treangleArr[((y + 1) * y + y * 2)/2] = value;
+    triangleArr.ref[((y + 1) * y + y * 2)/2] = value;
 }
 
 //FIX 
